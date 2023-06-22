c8b102ff79637d4f9d721782231de627fc0e6dc2
Yash <y.kataria@iitg.ac.in>
2023-06-22 10:22:47 +0530

    printing all stations

--- graph.cpp
+++ graph.cpp
@@ -5,22 +5,46 @@ using namespace std;
 
 class Graph {
     private:
-        unordered_map<int,vector<int>> vertices;
+        unordered_map<string,vector<string>> vertices;
 
     public:
-        void addVertex(int v) {
-            vertices[v]=vector<int>();
+        void addVertex(string v) {
+            vertices[v]=vector<string>();
         }
 
-        void addEdge(int src, int dest) {
+        void addEdge(string src, string dest) {
             //as travelling in both directions is possible, the graph is gonna be an undirected one
             vertices[src].push_back(dest);
             vertices[dest].push_back(src);
         }
+
+        void showStations() {
+            for(auto station: vertices) {
+                cout<<station.first<<endl;
+            }
+        }
+
+        void showMetroMap() {
+            for(auto station: vertices) {
+                cout<<station.first<<"->";
+                for(auto neighbour: station.second) cout<<neighbour<<", ";
+                cout<<endl;
+            }
+        }
 };
 
-int main(){
+void menu() {
 
+    Graph g;
+    g.addVertex("Govindpuri");
+    g.addVertex("Kalkaji Mandir");
+    g.addVertex("Lajpat Nagar");
+    g.addVertex("Nehru Enclave");
+    g.addVertex("IIT");
+    g.addVertex("Hauz Khas");
+    g.addVertex("IGI Terminal 1");
+    g.addVertex("Huda City Center");
+    
     cout<<"\n\t\t\t\t\t\tWELCOME TO METRO APP\t\t\t\t\t\t\n"<<endl;
     while(true) {
         cout<<"LIST OF ACTIONS:\n\n1. LIST ALL THE STATIONS IN THE MAP\n2. SHOW THE METRO MAP\n3. GET SHORTEST DISTANCE FROM A 'SOURCE' STATION TO A 'DESTINATION' STATION\n4. GET MINIMUM COST OF TRAVELLING FROM A 'SOURCE' STATION TO 'DESTINATION' STATION\n5. GET THE SHORTEST PATH FROM A 'SOURCE' STATION TO A 'DESTINATION' STATION\n6. GET THE PATH WITH THE MINIMUM COST TO TRAVEL FROM A 'SOURCE' STATION TO A 'DESTINATION' STATION\n7. EXIT\n\n";
@@ -31,12 +55,22 @@ int main(){
             cout<<"\n\nBYE! HAPPY JOURNEY\n\n"<<endl;
             break;
         }
-        else if(choice>=1 && choice<=6) {
+        else if(choice==1) {
+            cout<<endl<<endl;
+            g.showStations();
+            cout<<endl<<endl;
+        }
+        else if(choice>=2 && choice<=6) {
             cout<<"\n\nYour requested feature is under development.\n\n"<<endl;
         }
         else {
             cout<<"\n\nInvalid input! Please try again\n\n"<<endl;
         }
     }
+}
+
+int main(){
+
+    menu();
     return 0;
 }
