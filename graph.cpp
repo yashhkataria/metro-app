fb986a18036ca6b9be6493f07cbfab441ecb9b4f
Yash <y.kataria@iitg.ac.in>
2023-06-27 10:09:27 +0530

    distance and cost functionality added

--- graph.cpp
+++ graph.cpp
@@ -5,7 +5,8 @@ using namespace std;
 
 class Graph {
     private:
-        unordered_map<string,list<pair<string,int>>> vertices;
+        unordered_map<string,list<pair<string,int>>> verticesCost;
+        unordered_map<string,list<pair<string,int>>> verticesDistance;
         unordered_map<string,string> stationMapping;
 
     public:
@@ -23,24 +24,48 @@ class Graph {
             };
         }
 
-        void addVertex(string code) {
+        void addVertexCost(string code) {
             string name = stationMapping[code];
             if(!name.empty()) {
-                vertices[name] = list<pair<string,int>>();
+                verticesCost[name] = list<pair<string,int>>();
             }
             else {
                 cout<<"Invalid station code"<<endl;
             }
         }
 
-        void addEdge(string code1, string code2, int weight) {
+        void addVertexDistance(string code) {
+            string name = stationMapping[code];
+            if(!name.empty()) {
+                verticesDistance[name] = list<pair<string,int>>();
+            }
+            else {
+                cout<<"Invalid station code"<<endl;
+            }
+        }
+
+        void addEdgeCost(string code1, string code2, int weight) {
             //as travelling in both directions is possible, the graph is gonna be an undirected one
             string name1 = stationMapping[code1];
             string name2 = stationMapping[code2];
 
             if(!name1.empty() && !name2.empty()) {
-                vertices[name1].push_back(make_pair(name2,weight));
-                vertices[name2].push_back(make_pair(name1,weight));
+                verticesCost[name1].push_back(make_pair(name2,weight));
+                verticesCost[name2].push_back(make_pair(name1,weight));
+            }
+            else {
+                cout<<"Invalid station code"<<endl;
+            }
+        }
+
+        void addEdgeDistance(string code1, string code2, int weight) {
+            //as travelling in both directions is possible, the graph is gonna be an undirected one
+            string name1 = stationMapping[code1];
+            string name2 = stationMapping[code2];
+
+            if(!name1.empty() && !name2.empty()) {
+                verticesDistance[name1].push_back(make_pair(name2,weight));
+                verticesDistance[name2].push_back(make_pair(name1,weight));
             }
             else {
                 cout<<"Invalid station code"<<endl;
@@ -48,13 +73,29 @@ class Graph {
         }
 
         void showStations() {
-            for(auto station: vertices) {
+
+            //either graph can be used to print all the stations
+            for(auto station: verticesCost) {
                 cout<<station.first<<endl;
             }
         }
 
-        void showMetroMap() {
-            for(auto station: vertices) {
+        void showMetroMapCost() {
+
+            cout<<"Following are the fares of travelling from source to destination"<<endl;
+            for(auto station: verticesCost) {
+                cout<<station.first<<"->";
+                for(auto nbr: station.second) {
+                    cout<<"("<<nbr.first<<", "<<nbr.second<<"), ";
+                }
+                cout<<endl;
+            }
+        }
+
+        void showMetroMapDistance() {
+
+            cout<<"\nFollowing are the distances from source to destination"<<endl;
+            for(auto station: verticesDistance) {
                 cout<<station.first<<"->";
                 for(auto nbr: station.second) {
                     cout<<"("<<nbr.first<<", "<<nbr.second<<"), ";
@@ -75,12 +116,12 @@ class Graph {
 
             unordered_map<string,int> stationIndices;
             int index = 0;
-            for(auto &i: vertices) {
+            for(auto &i: verticesCost) {
                 string node = i.first;
                 stationIndices[node]=index++;
             }
 
-            int n = vertices.size()+1;
+            int n = verticesCost.size()+1;
             vector<int> dist(n,INT_MAX);
             set<pair<int,string>> s;
 
@@ -94,7 +135,7 @@ class Graph {
 
                 s.erase(s.begin());
 
-                for(auto nbr: vertices[stationName]) {
+                for(auto nbr: verticesCost[stationName]) {
                     int nbrIdx = stationIndices[nbr.first];
                     if(stationDist+nbr.second<dist[nbrIdx]) {
                         auto res = s.find(make_pair(dist[nbrIdx],nbr.first));
@@ -108,27 +149,90 @@ class Graph {
             }
             cout<<"\nThe minimum cost to travel from "<<srcStation<<" to "<<destStation<<" is "<<dist[stationIndices[destStation]]<<" rupees.\n\n";
         }
+
+        void minimumDistanceDjikstra(string srcCode,string destCode) {
+            
+            string srcStation = stationMapping[srcCode];
+            string destStation = stationMapping[destCode];
+
+            if(srcStation.empty() || destStation.empty()) {
+                cout<<"\nEither source or destination code is invalid\n\n";
+                return;
+            }
+
+            unordered_map<string,int> stationIndices;
+            int index = 0;
+            for(auto &i: verticesDistance) {
+                string node = i.first;
+                stationIndices[node]=index++;
+            }
+
+            int n = verticesDistance.size()+1;
+            vector<int> dist(n,INT_MAX);
+            set<pair<int,string>> s;
+
+            dist[stationIndices[srcStation]]=0;
+            s.insert(make_pair(0,srcStation));
+
+            while(!s.empty()) {
+                auto top = *(s.begin());
+                int stationDist = top.first;
+                string stationName = top.second;
+
+                s.erase(s.begin());
+
+                for(auto nbr: verticesDistance[stationName]) {
+                    int nbrIdx = stationIndices[nbr.first];
+                    if(stationDist+nbr.second<dist[nbrIdx]) {
+                        auto res = s.find(make_pair(dist[nbrIdx],nbr.first));
+                        if(res!=s.end()) {
+                            s.erase(res);
+                        }
+                        dist[nbrIdx]=stationDist+nbr.second;
+                        s.insert(make_pair(dist[nbrIdx],nbr.first));
+                    }
+                }
+            }
+            cout<<"\nThe shortest distance from "<<srcStation<<" to "<<destStation<<" is "<<dist[stationIndices[destStation]]<<" KM.\n\n";
+        }
 };
 
 void menu() {
 
-    Graph g;
-    g.addVertex("GP");
-    g.addVertex("KM");
-    g.addVertex("LN");
-    g.addVertex("NE");
-    g.addVertex("IIT");
-    g.addVertex("HK");
-    g.addVertex("IGIT1");
-    g.addVertex("HCC");
+    Graph gCost,gDistance;
+    gCost.addVertexCost("GP");
+    gCost.addVertexCost("KM");
+    gCost.addVertexCost("LN");
+    gCost.addVertexCost("NE");
+    gCost.addVertexCost("IIT");
+    gCost.addVertexCost("HK");
+    gCost.addVertexCost("IGIT1");
+    gCost.addVertexCost("HCC");
 
-    g.addEdge("GP","KM",20);
-    g.addEdge("KM","LN",25);
-    g.addEdge("LN","NE",35);
-    g.addEdge("NE","IIT",40);
-    g.addEdge("IIT","HK",20);
-    g.addEdge("IIT","IGIT1",50);
-    g.addEdge("HK","HCC",45);
+    gDistance.addVertexDistance("GP");
+    gDistance.addVertexDistance("KM");
+    gDistance.addVertexDistance("LN");
+    gDistance.addVertexDistance("NE");
+    gDistance.addVertexDistance("IIT");
+    gDistance.addVertexDistance("HK");
+    gDistance.addVertexDistance("IGIT1");
+    gDistance.addVertexDistance("HCC");
+
+    gCost.addEdgeCost("GP","KM",20);
+    gCost.addEdgeCost("KM","LN",25);
+    gCost.addEdgeCost("LN","NE",35);
+    gCost.addEdgeCost("NE","IIT",40);
+    gCost.addEdgeCost("IIT","HK",20);
+    gCost.addEdgeCost("IIT","IGIT1",50);
+    gCost.addEdgeCost("HK","HCC",45);
+
+    gDistance.addEdgeDistance("GP","KM",2);
+    gDistance.addEdgeDistance("KM","LN",2);
+    gDistance.addEdgeDistance("LN","NE",3);
+    gDistance.addEdgeDistance("NE","IIT",4);
+    gDistance.addEdgeDistance("IIT","HK",2);
+    gDistance.addEdgeDistance("IIT","IGIT1",5);
+    gDistance.addEdgeDistance("HK","HCC",4);
     
     cout<<"\n\t\t\t\t\t\tWELCOME TO METRO APP\t\t\t\t\t\t\n"<<endl;
     while(true) {
@@ -142,14 +246,26 @@ void menu() {
         }
         else if(choice==1) {
             cout<<endl<<endl;
-            g.showStations();
+            gCost.showStations();
             cout<<endl<<endl;
         }
         else if(choice==2) {
             cout<<endl<<endl;
-            g.showMetroMap();
+            gCost.showMetroMapCost();
+            gDistance.showMetroMapDistance();
             cout<<endl<<endl;
         }
+
+        else if(choice==3) {
+            string src,dest;
+            cout<<"Enter the SOURCE station code(starting point): ";
+            cin>>src;
+            cout<<"Enter the DESTINATION station code(ending point): ";
+            cin>>dest;
+
+            gDistance.minimumDistanceDjikstra(src,dest);
+        }
+
         else if(choice==4) {
             string src,dest;
             cout<<"Enter the SOURCE station code(starting point): ";
@@ -157,9 +273,9 @@ void menu() {
             cout<<"Enter the DESTINATION station code(ending point): ";
             cin>>dest;
 
-            g.minimumCostDjikstra(src,dest);
+            gCost.minimumCostDjikstra(src,dest);
         }
-        else if(choice==3 || choice==5 || choice==6) {
+        else if(choice==5 || choice==6) {
             cout<<"\n\nYour requested feature is under development.\n\n"<<endl;
         }
         else {
